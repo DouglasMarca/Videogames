eac5c2e2b6e2de77209f3b16d675f87cdbf3fafc
Douglas Marca <douglas.marca@uab.edu.bo>
2023-10-28 08:11:12 -0400

    Creando rama Develop

---
+++ VideogameDouglas/Videogames/Context/AplicacionContexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Videogames.EmailModel;
+using Videogames.UserModel;
+using Videogames.VideogamesModel;
+
+namespace Videogames.Context
+{
+    public class AplicacionContexto : DbContext
+    {
+        public AplicacionContexto
+            (DbContextOptions<AplicacionContexto> options)
+            : base(options) { }
+
+        public DbSet<Email> Email { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<Videogame> Videogame { get; set; }
+    }
+}
